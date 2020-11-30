b20ddbd47fbf6b786a67af65df8fd5024ecb337e
Serhii <logachovs555@gmail.com>
2020-12-01 00:29:42 +0200

    hw25 4/10

--- JS/hw25/source/js/dist/main.dev.js
+++ JS/hw25/source/js/dist/main.dev.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /*1. Запросить у пользователя его возраст и определить, кем он является: ребенком (0–2), подростком (12–18), взрослым (18_60) или пенсионером (60– ...) */
 
 /* let status = "in the plans"
@@ -16,23 +14,42 @@ console.log(`You are a ${status}.`); */
 /* let symbol = "";
 let number = +prompt("Enter a number from 0 to 9");
 switch(number) {
-    case 1: symbol = "!"; break; 
-    case 2: symbol = "@"; break;
-    case 3: symbol = "#"; break;
-    case 4: symbol = "$"; break;
-    case 5: symbol = "%"; break;
-    case 6: symbol = "^"; break;
-    case 7: symbol = "&"; break;
-    case 8: symbol = "*"; break;
-    case 9: symbol = "("; break;
-    case 10: symbol = ")"; break;
+        case 1: symbol = "!"; break; 
+        case 2: symbol = "@"; break;
+        case 3: symbol = "#"; break;
+        case 4: symbol = "$"; break;
+        case 5: symbol = "%"; break;
+        case 6: symbol = "^"; break;
+        case 7: symbol = "&"; break;
+        case 8: symbol = "*"; break;
+        case 9: symbol = "("; break;
+        case 10: symbol = ")"; break;
 }  
 console.log(`Key ${number} corresponds to the symbol ${symbol}.`); */
 
 /*3. Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры.
  */
-var number = +prompt("Please enter a three-digit number");
+
+/* let number = +prompt("Please enter a three-digit number");
+let ones = number % 10;
+let tens = (number - ones) / 10 % 10;
+let hundreds = (number - tens * 10 - ones) / 100;
+if (ones == tens && tens == hundreds)
+        { console.log(`All numbers ${ones}, ${tens}, ${hundreds} are the same`); }
+else if (ones == tens && tens != hundreds) 
+        { console.log(`Ones ${ones} and tens ${tens} are the same`); }
+else if (ones != tens && tens == hundreds) 
+        { console.log(`Tens ${tens} and hundreds ${hundreds} are the same`); }      
+else if (ones != tens && ones == hundreds) 
+        { console.log(`Ones ${ones} and hundreds ${hundreds} are the same`); }       
+else console.log(`There are no identical numbers`); */
+
 /*4. Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100. */
+
+/* let year = +prompt("Enter the year");
+if ( year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 ))
+{ console.log(`This year ${year} is a leap year`); }
+else console.log("This is not a leap year"); */
 
 /*5. Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
  */
@@ -47,3 +64,4 @@ var number = +prompt("Please enter a three-digit number");
 /*9. Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. После вопросов выведите пользователю количество набранных баллов. */
 
 /*10. Запросить дату (день, месяц, год) и вывести следующую за ней дату. Учтите возможность перехода на следующий месяц, год, а также високосный год.  */
+"use strict";
