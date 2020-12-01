5c2775be39f3b8f25c4e188bfc4ded38cbc0caf9
Serhii <logachovs555@gmail.com>
2020-12-01 20:01:28 +0200

    test

--- JS/hw25/source/js/dist/main.dev.js
+++ JS/hw25/source/js/dist/main.dev.js
@@ -46,7 +46,7 @@ else console.log(`There are no identical numbers`); */
 
 /*4. Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100. */
 
-/* let year = +prompt("Enter the year");
+/* let year = +prompt("Enter the year");g
 if ( year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 ))
 { console.log(`This year ${year} is a leap year`); }
 else console.log("This is not a leap year"); */
--- JS/hw25/source/js/main.js
+++ JS/hw25/source/js/main.js
@@ -45,7 +45,7 @@
 
 /*4. Запросить у пользователя год и проверить, високосный он или нет. Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100. */
 
-        /* let year = +prompt("Enter the year");
+        /* let year = +prompt("Enter the year");g
         if ( year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 ))
         { console.log(`This year ${year} is a leap year`); }
         else console.log("This is not a leap year"); */
