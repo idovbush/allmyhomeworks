1cb9c7e96554d31010a3429ca421fd00dd776b59
Serhio <logachovs555@gmail.com>
2020-12-01 16:10:33 +0200

    hw25 7/10

--- JS/hw25/source/js/dist/main.dev.js
+++ JS/hw25/source/js/dist/main.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*1. Запросить у пользователя его возраст и определить, кем он является: ребенком (0–2), подростком (12–18), взрослым (18_60) или пенсионером (60– ...) */
 
 /* let status = "in the plans"
@@ -54,6 +56,15 @@ else console.log("This is not a leap year"); */
 /*5. Запросить у пользователя пятиразрядное число и определить, является ли оно палиндромом.
  */
 
+/* let number = +prompt("Enter a five-digit number");
+let firstDigit = number % 10;
+let secondDigit = (number - firstDigit) / 10 % 10;
+let thirdDigit = (number - secondDigit * 10 - firstDigit) / 100 % 10;
+let fourthDigit = (number - thirdDigit * 100 - secondDigit * 10 - firstDigit) / 1000 % 10;
+let fifthDigit = (number - fourthDigit * 1000 - thirdDigit * 100 - secondDigit * 10 - firstDigit) / 10000;
+if ( firstDigit == fifthDigit && secondDigit == fourthDigit ) { console.log(`This number ${number} is polyindrome`) }
+else console.log(`This number ${number} is not a polyindrome`); */
+
 /*6. Написать конвертор валют. Пользователь вводит количество USD, выбирает, в какую валюту хочет перевести EUR, UAN или AZN, и получает в ответ соответствующую сумму. */
 
 /*7. Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой: от 200 до 300 – скидка будет 3%, от 300 до 500 – 5%, от 500 и выше – 7%.
@@ -61,7 +72,19 @@ else console.log("This is not a leap year"); */
 
 /*8. Запросить у пользователя длину окружности и периметр квадрата. Определить, может ли такая окружность поместиться в указанный квадрат. */
 
-/*9. Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. После вопросов выведите пользователю количество набранных баллов. */
+/* let lenght = +prompt("Enter the circumference");
+let perimeter = +prompt("Enter the perimeter of the square");
+const PI = 3.1415;
+if (lenght / 2 / PI <= perimeter / 8 ) { console.log("This circle fits into this square") } //сравниваем радиус окружности и половину стороны квадрата
+else console.log("This circle does not fit into this square"); */
 
+/*9. Задать пользователю 3 вопроса, в каждом вопросе по 3 варианта ответа. За каждый правильный ответ начисляется 2 балла. После вопросов выведите пользователю количество набранных баллов. */
+var points = 0;
+var firstQuestion = +prompt("Who was the first cosmonaut? 1-Gagarin, 2-Leonov, 3-Armstrong.");
+var secondQuestion = +prompt("What is the deepest ocean? 1-Pacific, 2-Atlantic, 3-Indian.");
+var thirdQuestion = +prompt("When did World War II start? 1-1945, 2-1939, 3-1941.");
+if (firstQuestion == 1) points += 2;
+if (secondQuestion == 3) points += 2;
+if (thirdQuestion == 2) points += 2;
+console.log("\u0412\u044B \u043D\u0430\u0431\u0440\u0430\u043B\u0438 ".concat(points, " \u0431\u0430\u043B\u043B\u0430(\u043E\u0432)."));
 /*10. Запросить дату (день, месяц, год) и вывести следующую за ней дату. Учтите возможность перехода на следующий месяц, год, а также високосный год.  */
-"use strict";
