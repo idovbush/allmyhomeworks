fb4ca47807638fe29546c98bbbc1501ba8e12f7f
Serhii <logachovs555@gmail.com>
2020-12-26 01:21:13 +0200

    hw31 start

--- JS/hw30/source/js/dist/main.dev.js
+++ JS/hw30/source/js/dist/main.dev.js
@@ -44,7 +44,43 @@
 // метод для печати (метод принимает строку и выводит текст соответствующим цветом; текст выводится до тех пор, пока в маркере есть чернила; один не пробельный символ – это 0,5% чернил в маркере).
 // Реализовать класс, описывающий заправляющийся маркер, унаследовав его от простого маркера и добавив метод для заправки маркера.
 // Продемонстрировать работу написанных методов.
+// class Marker {
+//     constructor(color, fullness) {
+//     this.color  = color;
+//     this.fullness = fullness;
+//     }
+//     drawText(text) {
+//         let printText = '';
+//         let textArr = text.split();
+//         console.log(textArr);
+//         for (let i = 0; i <= 199; i++) {
+//             printText += textArr[i];
+//             this.fullness -= 0.5;
+//         }
+//         console.log(printText);  
+//         return printText;  
+//     }
+// }
+// let myMarker = new Marker('green', 75);
+// let a = myMarker.drawText = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non quidem laborum aliquid fugit saepe architecto, unde itaque cumque distinctio eaque cupiditate, ea harum, voluptatum assumenda omnis quisquam debitis accusantium inventore.';
+// console.log(a);
+// myMarker;
 // 3) Реализовать класс Employee, описывающий работника, и создать массив работников банка.
 // Реализовать класс EmpTable для генерации HTML-кода таблицы со списком работников банка. Массив работников необходимо передавать через конструктор, а получать HTML-код с помощью метода getHtml().
 // Создать объект класса EmpTable и вывести на экран результат работы метода getHtml().
+// class Employee {
+//     constructor(name,position,salary) {
+//         this.name = name;
+//         this.salary = salary;
+//         this.position = position;
+//     }
+// }
+// function makeArr(number) {
+//     arrBank = [];
+//     for (let i = 0; i < number; i++) {
+//         arrBank.push(new Employee("asdds","jjjjjj",1233))
+//     }
+//     return arrBank;
+// }
+// console.log(makeArr(5));
 "use strict";
