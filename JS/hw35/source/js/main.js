4646dedf1e6781a9f423b80418d668ac5b79225f
Serhii <logachovs555@gmail.com>
2021-01-11 01:59:26 +0200

    start hw35

---
+++ JS/hw35/source/js/main.js
@@ -0,0 +1,12 @@
+//keyAPI= dabc6765d886817541a5cc898b76483b 
+//key= eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWJjNjc2NWQ4ODY4MTc1NDFhNWNjODk4Yjc2NDgzYiIsInN1YiI6IjVmZmI2OTliMjBhZjc3MDAzZDFiYzc2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4xWxlONQDvHKf0Ql3BWD8orLZxpE2VuR1xzyMkIRAyM
+
+// fetch("https://api.themoviedb.org/3/movie/550?api_key=dabc6765d886817541a5cc898b76483b")
+// .then(function (response) {
+//     return response.json();
+    
+// })
+// .then(function (myJson) {
+//     console.log(JSON.stringify(myJson));
+    
+// })
