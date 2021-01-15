a8d080b222b6ad435a2cdf7245eb0714b521865f
Serhii <logachovs555@gmail.com>
2021-01-16 01:22:27 +0200

    E2+1

--- JS/exam2/source/js/dist/main.dev.js
+++ JS/exam2/source/js/dist/main.dev.js
@@ -1,6 +1,11 @@
+// $(document).ready(function(){
+//   $('.wrapper').slick({
+//     //setting-name: setting-value
+//     dots: true,
+//     infinite: false,
+//     speed: 1000,
+//     vertical:true,
+//     verticalswiping: true,
+//   });
+// });
 "use strict";
-
-$(document).ready(function () {
-  $('.wrapper').slick({//setting-name: setting-value
-  });
-});
--- JS/exam2/source/js/main.js
+++ JS/exam2/source/js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-  $('.wrapper').slick({
-    //setting-name: setting-value
-  });
-});
+// $(document).ready(function(){
+//   $('.wrapper').slick({
+//     //setting-name: setting-value
+//     dots: true,
+//     infinite: false,
+//     speed: 1000,
+//     vertical:true,
+//     verticalswiping: true,
+
+//   });
+// });
