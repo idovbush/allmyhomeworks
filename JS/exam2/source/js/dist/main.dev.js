fc54853e21f5b6eb567320fd1a7da4132f7106b4
Serhii <logachovs555@gmail.com>
2021-01-17 07:05:52 +0200

    E2 +2

--- JS/exam2/source/js/dist/main.dev.js
+++ JS/exam2/source/js/dist/main.dev.js
@@ -1,6 +1,20 @@
 "use strict";
 
-// $(document).ready(function(){
+//$(document).ready(function() {
+// $('.header__burger').click(function() {
+//   console.log(123);
+//   $('.header__burger, .header__menu').toggleClass('active');
+//   console.log(456);
+// });
+//})
+$('.header__burger').click(function () {
+  $('.header__burger, .header__menu').toggleClass('active');
+  $('body').toggleClass('lock');
+});
+$('.header__menu').click(function () {
+  $('.header__burger, .header__menu').removeClass('active');
+  $('body').removeClass('lock');
+}); // $(document).ready(function(){
 //   $('.wrapper').slick({
 //     //setting-name: setting-value
 //     dots: true,
@@ -10,7 +24,21 @@
 //     verticalswiping: true,
 //   });
 // });
+
 $('[data-fancybox="gallery"]').fancybox({
   loop: true,
   buttons: ["close"]
-});
+}); // // Initialize the platform object:
+// var platform = new H.service.Platform({
+//   'apikey': '{BLcoALoeXiUexjUAjjA7KQre5GlOqMRtSQMlXPuYS0Y}'
+// });
+// // Obtain the default map types from the platform object
+// var maptypes = platform.createDefaultLayers();
+// // Instantiate (and display) a map object:
+// var map = new H.Map(
+//   document.getElementById('mapContainer'),
+//   maptypes.vector.normal.map,
+//   {
+//     zoom: 15,
+//     center: { lng: 10.4, lat: 10.51 }
+//   });
